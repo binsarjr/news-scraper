ff17f9952f10f713b5c84eab330b58f76043490b
binsarjr <binsarjr121@gmail.com>
2024-12-20 19:29:33 +0700

    feat: Enhance CNN spider with dynamic user-agent generation and update allowed domains

--- news_scraper/spiders/CNN.py
+++ news_scraper/spiders/CNN.py
@@ -1,5 +1,6 @@
 import scrapy, news_scraper.utils as myparser
 from news_scraper.items import NewsScraperItem
+import random
 
 
 from bs4 import BeautifulSoup
@@ -7,13 +8,21 @@ from bs4 import BeautifulSoup
 
 class CnnSpider(scrapy.Spider):
     name = "CNN"
-    allowed_domains = ["cnnindonesia.com"]
+    allowed_domains = ["cnnindonesia.com","www.cnnindonesia.com"]
     handle_httpstatus_list = [301]
     custom_settings = {
         "ITEM_PIPELINES": {
             "news_scraper.pipelines.DateFilterPipeline": 110,
         },
+        "DOWNLOADER_MIDDLEWARES": {
+            "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+            "scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware": 500,
+        },
     }
+
+
+    def generateUserAgent(self):
+        return "Chrome/103.0.5060.129 Mobile Safari/537.36" + str(random.randint(1,100))
 
     def start_requests(self):
         keywords = self.keyword.split(",")
@@ -21,7 +30,9 @@ class CnnSpider(scrapy.Spider):
             # for cnn
             yield scrapy.Request(
                 url=f"https://www.cnnindonesia.com/api/v2/search?query={keyword}&start=0&limit=10",
-                headers={"referer": None},
+                headers={
+                      'user-agent':self.generateUserAgent(),
+                    },
                 callback=self.parse_search,
                 meta={"keyword": keyword, "source": "cnnindonesia.com"},
             )
@@ -38,12 +49,18 @@ class CnnSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=f"https://www.cnnindonesia.com/api/v2/search?query={response.meta['keyword']}&start={response.meta['start']}&limit=10",
                 callback=self.parse_search,
+                headers={
+                      'user-agent':self.generateUserAgent(),
+                    },
                 meta=response.meta,
             )
 
         for article in data["data"]:
             yield scrapy.Request(
                 url=article["url"],
+                headers={
+                      'user-agent':self.generateUserAgent(),
+                    },
                 callback=self.parse,
                 meta=response.meta,
             )
@@ -53,6 +70,9 @@ class CnnSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=response.headers["location"].decode("utf-8"),
                 callback=self.parse,
+                headers={
+                      'user-agent':self.generateUserAgent(),
+                    },
                 meta=response.meta,
             )
             return
